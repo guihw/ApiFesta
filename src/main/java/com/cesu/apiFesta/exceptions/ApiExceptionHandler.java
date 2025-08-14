0c88a5527d5307b728216e5205a903c9d26c257c
Guilherme Santos <guilhermess2005g@gmail.com>
2025-08-14 18:32:53 -0300

    Tratamento de erro para input inválido de parâmetro

--- src/main/java/com/cesu/apiFesta/exceptions/ApiExceptionHandler.java
+++ src/main/java/com/cesu/apiFesta/exceptions/ApiExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -11,5 +12,10 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(PessoaApiEmptyException.class)
     public ResponseEntity<String> ApiVaziaError(PessoaApiEmptyException exception){
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> PathVariableTipoErradoError(MethodArgumentTypeMismatchException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Parâmetro id deve ser um inteiro");
     }
 }
